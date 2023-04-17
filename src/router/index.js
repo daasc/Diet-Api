5618fc67ced0f048428f5855db25d4227914e5c5
daasc <paulojhol@gmail.com>
2023-04-17 12:16:01 -0300

    feat: add authorize in the route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
+const authorizer = require('../middleware/authorize');
+
 const calendar = require('./calendar');
 const user = require('./user');
 
 const router = Router();
 
-router.use('/calendar', calendar);
+router.use('/calendar', authorizer, calendar);
 router.use('/user', user);
 
 router.get('/', async (req, res) => {
